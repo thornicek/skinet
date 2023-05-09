c6a1d05fa8657a84433e75f2525fdf6374f37a27
Hornicek <tomas.hornicek@adastragrp.com>
2023-05-09 09:33:57 -0400

    first commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
