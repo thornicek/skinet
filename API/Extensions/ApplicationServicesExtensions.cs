e09f7844bf9a3255153a2e60327177cc01f95e8b
Hornicek <tomas.hornicek@adastragrp.com>
2023-05-25 17:33:13 -0400

    End of section 6

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -40,6 +40,14 @@ namespace API.Extensions
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
             
             return services;
         }
